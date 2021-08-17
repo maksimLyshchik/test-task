d8fa14203cc9be9f4be706668c895438e39b29d9
maksim.lyshchik <maksim.lyshchik@intexsoft.by>
2021-08-17 09:47:39 +0300

    Добавлен сброс выделенных задачь после изменения их статуса. Исправление лишних свойств.

--- src/components/SideBarRight/EditorTasks/EditorTasks.js
+++ src/components/SideBarRight/EditorTasks/EditorTasks.js
@@ -6,6 +6,7 @@ import { COMPLETED, IN_PROGRESS, REJECTED } from '../../../common/constants/cons
 import { completedTask, rejectedTask, todoTask } from '../../../store/tasks/actionsTasks';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectMarkedTask } from '../../../store/selectedEntity/selectorSelects';
+import { setSelectTask } from '../../../store/selectedEntity/actionsSelects';
 import s from './EditTasks.module.css';
 
 export const EditorTasks = () => {
@@ -15,21 +16,27 @@ export const EditorTasks = () => {
   const handleRejectedTask = useCallback(() => {
 
     markTasksId.map(id => dispatch(rejectedTask(id)));
+
+    markTasksId.map(id => dispatch(setSelectTask({[id]: false})));
   }, [dispatch, markTasksId]);
 
   const handleCompletedTask = useCallback(() => {
 
     markTasksId.map(id => dispatch(completedTask(id)));
+
+    markTasksId.map(id => dispatch(setSelectTask({[id]: false})));
   }, [dispatch, markTasksId]);
 
   const handleTodoTask = useCallback(() => {
 
     markTasksId.map(id => dispatch(todoTask(id)));
+
+    markTasksId.map(id => dispatch(setSelectTask({[id]: false})));
   }, [dispatch, markTasksId]);
 
   return (
     <div className={s.tasksEditor}>
-      <span className={s.tasksEditor__name}>Edit tasks</span>
+      <span className={s.tasksEditor__name}>Tasks manager</span>
       <div className={s.tasksEditor__panel}>
         <Button color={WARNING} onClick={handleRejectedTask} >
           <Icon type={REJECTED} />
